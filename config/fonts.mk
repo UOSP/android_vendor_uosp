8e600427a46c47f21dcaf1d86eae91fc47eb8925
faham1997 <nafidfaham08@gmail.com>
2020-10-26 21:47:40 +0600

    uosp : Build Font overlays

--- config/fonts.mk
+++ config/fonts.mk
@@ -4,3 +4,12 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(FONT_PATH)/ttf,$(TARGET_
 
 PRODUCT_COPY_FILES += \
     $(FONT_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Font overlays
+PRODUCT_PACKAGES += \
+    FontArbutusSourceOverlay \
+    FontArvoLatoOverlay \
+    FontKaiOverlay \
+    FontRubikRubikOverlay \
+    FontSamOverlay \
+    FontVictorOverlay
